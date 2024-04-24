0da4da62866858712f96e460a40e1969b8001ba6
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-04-24 20:12:58 +0700

    Initial Commit

---
+++ HW9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW9
+//
+//  Created by Павел Градов on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
